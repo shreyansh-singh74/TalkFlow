5ecf1038c287dfa3f87d74f04f9cdd5a11d1b8a0
shreyansh <shreyansh.workid@gmail.com>
2025-08-26 10:59:35 +0530

    add authentication views and layout components

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+
+export default function Page() {
+  console.log("Sign in Page");
+  return (
+        <SignInView />
+  );
+}
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+
+export default function Page() {
+  console.log("Sign up Page");
+  return (
+        <SignUpView />
+  );
+}
