82dccd84e3edc50140e5ccf39364167f81da2979
shreyansh <shreyansh.workid@gmail.com>
2025-09-03 11:47:22 +0530

    agents data table

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -2,12 +2,26 @@
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { DataTable } from "../components/data-table";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 
 export const AgentsView = () => {
+
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data} columns={columns}/>
+      {data.length === 0 && (
+        <EmptyState 
+          title="Create Your First Agent"
+          description="Create an agent to join your meeings. Each agent will follow your instrucions and can interac with participants during the call."
+        />
+      )}
+    </div>
+  );
 };
 
 export const AgentsViewLoading = () => {
