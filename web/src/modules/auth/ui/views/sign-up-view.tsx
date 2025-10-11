114313b45fa5b1456b935b7bcf1cfbd37fc7d0db
shreyansh <shreyansh.workid@gmail.com>
2025-10-11 17:09:50 +0530

    Revert "Rename project from LinguaLive to TalkFlow, updating references in backend and frontend files. Modify health check message and update UI elements to reflect new branding."
    This reverts commit 217bf97bde2ab2ba8dae71922d4728e7e3aa1ce5.

--- web/src/modules/auth/ui/views/sign-up-view.tsx
+++ web/src/modules/auth/ui/views/sign-up-view.tsx
@@ -237,7 +237,7 @@ export const SignUpView = () => {
           </Form>
           <div className="bg-radial from-sidebar-accent to-sidebar p-10 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
             <img src="/logo.svg" alt="Image" className="h-[92px] w-[92px]" />
-            <p className="text2xl font-semibold text-white">TalkFlow</p>
+            <p className="text2xl font-semibold text-white">LinguaLive</p>
           </div>
         </CardContent>
       </Card>
